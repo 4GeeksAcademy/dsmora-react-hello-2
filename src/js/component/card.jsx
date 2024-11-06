555a3842264a52f6550a81d0e1ca94a6c30b4f28
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2024-11-06 16:58:09 +0000

    wip

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -7,34 +7,6 @@ import React, { useEffect, useState } from "react";
 // condicion ? respuesta si true : respuesta si false 
 
 export const Card = ({ title, description, image, stock }) => {
-    const [active, setActive] = useState(false);
-
-    console.log(active, title);
-
-    // Mountinig
-    // Updating
-    // Unmounting
-
-    useEffect(() => {
-        console.log('Mounting');
-        console.log('Updating');
-        if (active) alert('Estamos actualizando')
-
-        return () => {
-            console.log('UnMounting')
-        }
-    }, [active]);
-
-    const handlerClickActive = (e) => {
-        e.preventDefault();
-        setActive(true)
-    };
-
-    const handlerClickDeActive = (e) => {
-        e.preventDefault();
-        setActive(false)
-    };
-
 
     return (
         <div className={`card ${active ? 'bg-danger-subtle' : ''}`} style={{ width: '18rem' }}>
@@ -44,8 +16,7 @@ export const Card = ({ title, description, image, stock }) => {
             <div className="card-body">
                 <h5 className="card-title">{title} stock: {stock}</h5>
                 <p className="card-text">{description}</p>
-                <a href="#" className="btn btn-primary" onClick={handlerClickActive}>Active</a>
-                <a href="#" className="btn btn-primary" onClick={handlerClickDeActive}>Deactive</a>
+                <a href="#" className="btn btn-primary">Active</a>
             </div>
         </div>
     )
