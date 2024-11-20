1e5f6ac0366cca871887aab30351b3352f058418
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2024-11-20 18:28:32 +0000

    todo list con fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //create your first component
 const Home = () => {
 
-	const [counter, setCounter] = useState(0);
+	const [inputValue, setInputValue] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/todo/users/dsmora')
+			.then(resp => resp.json())
+			.then(respJson => {
+				console.log(respJson)
+				console.log(respJson.todos)
+				const serverTodos = respJson.todos;
+				setTodos(serverTodos);
+			})
+	}, [])
+
+	const createTodo = async (task) => {
+		await fetch('https://playground.4geeks.com/todo/todos/dsmora', {
+			method: 'POST',
+			body: JSON.stringify({
+				"label": task,
+				"is_done": false
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(resp => resp.json())
+			.then(respJson => {
+				const newTodos = [...todos, respJson];
+				setTodos([...newTodos])
+			});
+	}
+
+
+	const handleOnChange = (evt) => {
+		if (evt.key === 'Enter' && evt.target.value.trim() !== '') {
+			createTodo(evt.target.value)
+			setInputValue('')
+		}
+	}
+
+	const handleOnDelete = (deleteIndex) => {
+		const newTodos = todos.filter((_, index) => index !== deleteIndex);
+		setTodos([...newTodos])
+	}
+
 	return (
-		<div className="container-fluid p-4 text-center">
-			<h1>
-				Home, contador igual a: {counter}
-			</h1>
-			<button onClick={() => setCounter(counter + 1)}>
-				Increment
-			</button>
+		<div className="container">
+			<div className="todos p-2">
+				<div className="input-group input-group-sm mb-3 py-3 px-5">
+					<input value={inputValue}
+						type="text"
+						className="form-control"
+						onChange={evt => setInputValue(evt.target.value)}
+						onKeyDown={handleOnChange} placeholder="New Task" />
+				</div>
+				<div className="container text-center px-5">
+					<div className="row row-cols-2">
+						{
+							todos.map((item, index) => (
+								<div className="col" key={item.id}>
+									<div className="todoCard">
+										{item.label}
+										<i className="fa fa-trash ms-4" onClick={() => handleOnDelete(index)}></i>
+									</div>
+								</div>
+							))
+						}
+					</div>
+				</div>
+				<h6 className="text-center">
+					Hay {todos.length} tareas pendientes
+				</h6>
+			</div>
+
 		</div>
+
 	);
 };
 
